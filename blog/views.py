1d1fcfda2250f3dfa7db05c46248fd495403871f
GOLLER ALESSANDRO <alessandro.goller@gmail.com>
2022-08-09 08:14:34 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import posts
 
 # Create your views here.
+import logging
+logger = logging.getLogger(__name__)
+
+
+logger.debug("Got %d posts", len(posts))
