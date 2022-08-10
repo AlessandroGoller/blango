a41c27b56f9a25aea9344a159192af624563b1a8
GOLLER ALESSANDRO <alessandro.goller@gmail.com>
2022-08-10 11:42:16 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import posts
+#from django.views.decorators.cache import cache_page
 
 # Create your views here.
 import logging
 logger = logging.getLogger(__name__)
 
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 logger.debug("Got %d posts", len(posts))
